18f3726e09ac8525d3a65b9f665798103f4f7746
Eryk <s31954@pjwstk.edu.pl>
2026-03-25 20:44:51 +0100

    Student and Employee implementaion

---
+++ DeviceRentalService/Employee.cs
@@ -0,0 +1,9 @@
+﻿using DeviceRentalService.Enums;
+
+namespace DeviceRentalService;
+
+public class Employee(string firstname, string lastname)
+    : User(firstname, lastname, UserType.Employee) {
+
+    public override int GetMaxRentals() => 5;
+}
---
+++ DeviceRentalService/Student.cs
@@ -0,0 +1,8 @@
+﻿using DeviceRentalService.Enums;
+namespace DeviceRentalService;
+
+public class Student(string firstname, string lastname) 
+    : User(firstname, lastname, UserType.Student) {
+
+    public override int GetMaxRentals() => 2;
+}
