2abfb048a177e0a22b4d870a7416a2d8f693f240
Eryk <s31954@pjwstk.edu.pl>
2026-03-25 22:06:34 +0100

    created IRentalService interface and boilerplate implementation in RentalService

---
+++ DeviceRentalService/Services/IRentalService.cs
@@ -0,0 +1,17 @@
+﻿using DeviceRentalService.Enums;
+using DeviceRentalService.Models;
+namespace DeviceRentalService.Services;
+
+public interface IRentalService {
+    public void CreateUser(string firstName, string lastName, UserType userType);
+    public void RentDevice(User user, Device device, DateTime from, DateTime to);
+    public List<Rental> GetUserRentals(User user);
+    public double ReturnDevice(User user, Device device, DateTime returnDate); 
+    public List<Rental> GetOverdueRentals();
+    public string GenerateRaport();
+    public void CreateDevice();
+    public List<Device> GetAllDevices();
+    public List<Device> GetAvailableDevices();
+    public void ChangeDeviceStatus(Device device,  DeviceStatus newStatus);
+    public DeviceStatus GetDeviceStatus(Device device);
+}
---
+++ DeviceRentalService/Services/RentalService.cs
@@ -0,0 +1,51 @@
+﻿using DeviceRentalService.Models;
+using DeviceRentalService.Enums;
+namespace DeviceRentalService.Services;
+
+public class RentalService : IRentalService{
+
+    public void CreateUser(string firstName, string lastName, UserType userType) {
+        throw new NotImplementedException();
+    }
+
+    public void RentDevice(User user, Device device, DateTime from, DateTime to) {
+        throw new NotImplementedException();
+    }
+
+    public List<Rental> GetUserRentals(User user) {
+        throw new NotImplementedException();
+    }
+
+    public double ReturnDevice(User user, Device device, DateTime returnDate) {
+        throw new NotImplementedException();
+    }
+
+    public List<Rental> GetOverdueRentals() {
+        throw new NotImplementedException();
+    }
+
+    public string GenerateRaport() {
+        throw new NotImplementedException();
+    }
+
+    public void CreateDevice() {
+        throw new NotImplementedException();
+    }
+
+    public List<Device> GetAllDevices() {
+        throw new NotImplementedException();
+    }
+
+    public List<Device> GetAvailableDevices() {
+        throw new NotImplementedException();
+    }
+
+    public void ChangeDeviceStatus(Device device, DeviceStatus newStatus) {
+        throw new NotImplementedException();
+    }
+
+    public DeviceStatus GetDeviceStatus(Device device) {
+        throw new NotImplementedException();
+    }
+    
+}
